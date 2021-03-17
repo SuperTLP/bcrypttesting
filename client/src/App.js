ff332ef08d86576dd3b38ee123eb83990f16e325
unknown <riku.p.mattila@gmail.com>
2021-03-17 23:41:32 +0200

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import {BrowserRouter, Route} from 'browser-router'
+
 
 function App() {
+  const [messages, setCount] = usestate(0)
+  setCount(messages=[])
   return (
     <div className="App">
+
+      <BrowserRouter>
+      </BrowserRouter>
+
+
     </div>
   );
 }
